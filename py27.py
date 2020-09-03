6ec77bdaa8e3a35d3d9aa951014c720b86cd6d39
Murilo Santos Lopes Carvalho <70426766+AkiraSendoh7@users.noreply.github.com>
2020-09-03 00:22:28 -0300

    Delete py27.py

--- py27.py
+++
@@ -1,8 +0,0 @@
-def number(n):
-    if (n%2 == 0):
-        print("O número é par")
-    else: 
-        print("O número é impar")
-
-numero = int(input("Digite o valor do número: "))
-number(numero)
