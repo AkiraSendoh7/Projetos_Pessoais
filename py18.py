bb3d786dc8e9a50e4229516cea43e411a225421d
Murilo Santos Lopes Carvalho <70426766+AkiraSendoh7@users.noreply.github.com>
2020-09-02 23:58:25 -0300

    Delete py18.py

--- py18.py
+++
@@ -1,6 +0,0 @@
-def personal_details():
-    name, age = "Murilo", 16
-    address = "São José dos Campos, São Paulo, Brasil"
-    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
-
-personal_details()
